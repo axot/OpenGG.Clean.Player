c7c6cf9678174b4c5917d5d6e99ea4abc68c41b1
Zheng Shao <axot@msn.com>
2013-11-04 20:16:37 +0900

    the 1.31 version on Chrome Store seems not working anymore, so I switch to 1.30 version feteched from http://userscripts.org/scripts/version/120679/534139.user.js

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ OpenGG.Clean.Player
 ===================
 
 # Description
-
 A safari extension to prevent sending user private data via video flash player(Tudou & Youku). The orinial Chrome version is available on [Chrome Web Store](https://chrome.google.com/webstore/detail/openggcleanplayer/doleffkdbkfeokcanjaagploacdflcff)
 
 # License
--- opengg_clean_player.safariextension/opengg.js
+++ opengg_clean_player.safariextension/opengg.js
@@ -2,14 +2,14 @@
 // @name           OpenGG.Clean.Player
 // @namespace      http://OpenGG.me
 // @description    OpenGG.Clean.Player
-// @version        1.31
+// @version        1.30
 // @updateURL      https://userscripts.org/scripts/source/120679.meta.js
 // @downloadURL    https://userscripts.org/scripts/source/120679.user.js
 // @match          http://*/*
 // @match          https://*/*
 // @exclude        http://www.kuaipan.cn/*
 // ==/UserScript==
-function main() {
+(function () {
     //Goddamn sina weibo.
     //'use strict';
     var Global = this;
@@ -60,7 +60,7 @@ function main() {
             }
         ],
         TIPS_HOLDER: '#miniheader,#gTop',
-        TIPS: '<div class="tips_container">OpenGG.Clean.Player \u5df2\u505c\u6b62&emsp;<a href="http://opengg.me/916/how-dreams-die/" style="color:red" title="\u7406\u60f3\u662f\u5982\u4f55\u5931\u53bb\u7684" target="_blank">\u8be6\u60c5</a><a class="tips_close" href="#" style="color:red">X</a></div>',
+        TIPS: '<div class="tips_container">OpenGG.Clean.Player \u5DF2\u542F\u7528&emsp;<span class="tips_toggleWide">\u5bbd\u5c4f/\u7a84\u5c4f&emsp;</span><a href="http://opengg.me/781/opengg-clean-player/" style="color:blue" target="_blank">\u53CD\u9988</a>&emsp;<a href="http://opengg.me/donation/" style="color:red" title="\u6211\u8981\u6350\u52A9\u6B64\u9879\u76EE" target="_blank">\u6350\u52A9</a><a class="tips_close" href="#" style="color:red">X</a></div>',
         STYLE: '.playBox_thx #player.player,.playBox_thx #player.player object{min-height:' + Math.max(Global.innerHeight * 0.6, 580) + 'px !important}.tips_container{position:absolute;top:3em;padding:1em 2em;right:50px;color:green;opacity:0.4;background:#ddd;z-index:999999}.tips_container:hover{opacity:0.8}.tips_container .tips_toggleWide{color:red;cursor:pointer;display:none}.tips_close{position:absolute;right:3px;top:3px}',
         NODEINSERTED_HACK: '@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}',
         TOGGLE_BTN: '.tips_container .tips_toggleWide'
@@ -115,6 +115,92 @@ function main() {
             return false;
         }
     };
+    var STORE;
+    (function(){
+        var isStorage = true;
+        if(!Global.localStorage){
+            isStorage = false;
+        }else{
+            try{
+                var key = String(Math.random());
+                localStorage.setItem(key,'test');
+                if(localStorage.getItem(key)!=='test'){
+                    throw 'not equal';
+                }
+                localStorage.removeItem(key);
+            }catch(e){
+                isStorage=false;
+            }
+        }
+        STORE = {
+            getItem: function(key){
+                if(isStorage){
+                    return localStorage.getItem(key);
+                }
+            },
+            setItem: function(key, value){
+                if(isStorage){
+                    localStorage.setItem(key, value);
+                }
+            }
+        };
+    })();
+    function onLoad(tagNameList, fn) {
+        var lowerTagNameList = [];
+        UTIL.forEach(tagNameList, function(a){
+            lowerTagNameList.push(a.toLowerCase());
+        });
+        function onDOMNodeInsertedHandler(e) {
+            var target = e.target;
+            if (target.nodeType === 1 && lowerTagNameList.indexOf(target.nodeName.toLowerCase())!==-1) {
+                fn(target);
+            }
+        }
+        function onAnimationStartHandler(e) {
+            if (e.animationName === 'nodeInserted') {
+                var target = e.target;
+                if (target.nodeType === 1 && lowerTagNameList.indexOf(target.nodeName.toLowerCase())!==-1) {
+                    fn(target);
+                }
+            }
+        }
+        function animationNotSupported(){
+            var style = document.createElement('div').style;
+            var arr = ['animation', 'MozAnimation', 'webkitAnimation', 'OAnimation'];
+            for(var i =0;i<arr.length;++i){
+                if( arr[i] in style){
+                    return false;
+                }
+            }
+            return true;
+        }
+        /* animationstart not invoked in background tabs of chrome 21 */
+        var all = document.querySelectorAll(lowerTagNameList.join(','));
+        for(var i=0;i<all.length;++i){
+            fn(all[i]);
+        }
+        UTIL.addCss(CONSTANTS.NODEINSERTED_HACK);
+        /*Firefox*/
+        document.body.addEventListener('animationstart', onAnimationStartHandler, false);
+        /*/Firefox*/
+        /*Chrome*/
+        document.body.addEventListener('webkitAnimationEnd', onAnimationStartHandler, false);
+        /*/Chrome*/
+        /*Opera 12+*/
+        document.body.addEventListener('oAnimationStart', onAnimationStartHandler, false);
+        /*/Opera 12+*/
+        /*IE, but I never tested this*/
+        document.body.addEventListener('msAnimationStart', onAnimationStartHandler, false);
+        /*/IE, but I never tested this*/
+        if (animationNotSupported()) {
+            /*Old fashion, slower maybe*/
+            document.body.addEventListener('DOMNodeInserted', onDOMNodeInsertedHandler, false);
+            var matches = document.body.querySelectorAll(lowerTagNameList.join(','));
+            UTIL.forEach(matches, function (elem) {
+                fn(elem);
+            });
+        }
+    }
     function tips() {
         var holder = document.body.querySelector(CONSTANTS.TIPS_HOLDER);
         if (holder) {
@@ -133,6 +219,12 @@ function main() {
             holder.appendChild(div);
             UTIL.addCss(CONSTANTS.STYLE);
         }
+    }
+    function share(elem) {
+        var pairs = CONSTANTS.SHARES;
+        UTIL.forEach(pairs, function (item) {
+            elem.value = elem.value.replace(item.find, item.replace);
+        });
     }
     function setTHX(opt){
         var player = document.querySelector('object#movie_player');
@@ -161,15 +253,98 @@ function main() {
     }
     var CONTROLLER = [
         {
+            host: '.',
+            fn: function () {
+                var known = [];
+                onLoad(CONSTANTS.PLAYER_DOM, function (elem) {
+                    var attrs = ['data', 'src'];
+                    var players = CONSTANTS.PLAYERS;
+                    var reloaded = false;
+                    if(known.indexOf(elem)!==-1){
+                        return;
+                    }
+                    UTIL.forEach(attrs, function (attr) {
+                        UTIL.forEach(players, function (player) {
+                            var find = player.find;
+                            var replace = player.replace;
+                            var value = elem[attr];
+                            var movie = elem.querySelector('param[name="movie"]');
+
+                            if(movie&&movie.value){
+                                movie.value = movie.value.replace(find,replace);
+                                reloaded = true;
+                            }
+                            if (value && find.test(value)) {
+                                var nextSibling = elem.nextSibling;
+                                var parentNode = elem.parentNode;
+                                var clone = elem.cloneNode(true);
+                                clone[attr] = value.replace(find, replace);
+                                parentNode.removeChild(elem);
+                                parentNode.insertBefore(clone, nextSibling);
+                                //Baidu tieba shit.
+                                if(clone && getComputedStyle(clone).display==='none' && clone.style){
+                                    clone.style.display='block';
+                                }
+                                reloaded = true;
+                            }
+                        });
+                    });
+                    if(reloaded){
+                        known.push(elem);
+                    }
+                });
+            }
+        },
+        {
             host: 'youku.com',
             fn: function () {
+                var matches = document.body.querySelectorAll(CONSTANTS.SHARE_DOM);
+                UTIL.forEach(matches, share);
+
                 tips();
+
+                if(STORE.getItem('THX')==='on'){
+                    setTHX(STORE.getItem('THX'));
+                }
+
+                var toggle = document.body.querySelector(CONSTANTS.TOGGLE_BTN);
+                if(toggle){
+                    toggle.style.display='inline';
+                    toggle.addEventListener('click',function(){
+                        STORE.setItem('THX',STORE.getItem('THX')==='on'?'off':'on');
+                        setTHX(STORE.getItem('THX'));
+                    },false);
+                }
             }
         },
         {
             host: 'tudou.com',
             fn: function () {
+                UTIL.proxy(function(Global, imports){
+                    function hack(){
+                        var TUI_copyToClip = Global.TUI&&Global.TUI.copyToClip;
+                        if(TUI_copyToClip&&TUI_copyToClip.toString().indexOf('arguments')===-1){
+                            Global.TUI.copyToClip = function () {
+                                var matches = document.body.querySelectorAll(imports.selector);
+                                UTIL.forEach(matches, share);
+                                TUI_copyToClip.apply(Global.TUI, arguments);
+                            };
+                            clearInterval(inter);
+                        }
+                    }
+                    var inter = setInterval(hack,100);
+                    try{
+                        Global.playerEx.event.fire('scale',[true]);
+                    }catch(e){}
+                },{
+                    selector: CONSTANTS.SHARE_DOM
+                });
                 tips();
+                var tudouPlayer = document.body.querySelector('#playerObject');
+                var normalDom = document.querySelector('.normal');
+                if (tudouPlayer && normalDom) {
+                    normalDom.className = normalDom.className.replace('normal','widescreen');
+                }
             }
         }
     ];
@@ -181,8 +356,4 @@ function main() {
         }
     }
     UTIL.forEach(CONTROLLER, PROC);
-}
-
-var script = document.createElement('script');
-script.textContent = '(' + main.toString() + ')(window);';
-document.body.appendChild(script);
+})();
